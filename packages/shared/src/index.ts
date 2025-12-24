22c713833b8a3c637dc0fea4c4f9af4b321a6a4d
Mark Burggraf <markb@mantisbible.com>
2025-12-24 07:15:41 -0800

    refactor: update shared package exports for markdown song format

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -4,8 +4,10 @@ export * from './types';
 // Hooks
 export * from './hooks';
 
-// Services
-export * from './services';
-
 // Utils
-export * from './utils';
+export {
+  parseSongMarkdown,
+  buildSongMarkdown,
+  songToSlides,
+} from './utils/markdown-song';
+export { getContrastColor } from './utils/colors';
